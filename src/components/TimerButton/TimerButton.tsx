5bbb689320cee11170edca8f252251e0eb3ed331
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-08-19 23:58:16 +0500

    Created PropTypes, Added buttonAction props to button onClick attribute and buttonValue props to p tag, added type from types folder to component.

--- src/components/TimerButton/TimerButton.tsx
+++ src/components/TimerButton/TimerButton.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./TimerButton.css";
-const TimerButton = () => {
+import { Props } from "../types";
+
+const TimerButton: React.FC<Props> = ({ buttonAction, buttonValue }) => {
   return (
-    <div className="button-container">
-      <p className="button-value"></p>
+    <div className="button-container" onClick={() => buttonAction()}>
+      <p className="button-value">{buttonValue}</p>
     </div>
   );
 };
+
+TimerButton.propTypes = {
+  buttonAction: PropTypes.func.isRequired,
+  buttonValue: PropTypes.string.isRequired,
+};
+
 export default TimerButton;
